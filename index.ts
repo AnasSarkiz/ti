8ea431711f584068228e6e7a6102b93d67ca35d1
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2026-08-26 20:32:18 +0530

    Add MSPM0L1306 chip and reference subcircuit (#119)
    * Add MSPM0L1306 chip and reference subcircuit
    
    * Add MSPM0L1306 schematic snapshot
    
    * Add original MSPM0L1306 schematic reference
    
    * Align MSPM0L1306 ROSC resistor and SWD connector
    
    * Align MSPM0L1306 reset capacitor with the NRST junction
    
    * Update tscircuit and refresh MSPM0L1306 schematic snapshot

--- index.ts
+++ index.ts
@@ -25,6 +25,7 @@ import {
   MSP430G2230ID,
   MSP430G2332IPW20,
   MSP430F5229,
+  MSPM0L1306,
   MSPM33C3x,
   MSPM0G3507,
   SN65HVD1473,
@@ -74,6 +75,7 @@ import { ClockBuffer_LMK1C1104 } from "./lib/subcircuits/ClockBuffer_LMK1C1104.c
 import { AudioAmplifier_TAS2505 } from "./lib/subcircuits/AudioAmplifier_TAS2505.circuit.tsx";
 import { TargetSocket_MSPTS430D8 } from "./lib/subcircuits/TargetSocket_MSPTS430D8.circuit.tsx";
 import { BluetoothAudioHost_MSP430F5229 } from "./lib/subcircuits/BluetoothAudioHost_MSP430F5229.circuit.tsx";
+import { Microcontroller_MSPM0L1306 } from "./lib/subcircuits/Microcontroller_MSPM0L1306.circuit.tsx";
 import { Microcontroller_MSPM0G3507 } from "./lib/subcircuits/Microcontroller_MSPM0G3507.circuit.tsx";
 import { Microcontroller_MSPM33C3x } from "./lib/subcircuits/Microcontroller_MSPM33C3x.circuit.tsx";
 import { LEDDriver_TLC59116 } from "./lib/subcircuits/LEDDriver_TLC59116.circuit.tsx";
@@ -128,6 +130,7 @@ export {
   AudioAmplifier_TAS2505,
   TargetSocket_MSPTS430D8,
   BluetoothAudioHost_MSP430F5229,
+  Microcontroller_MSPM0L1306,
   Microcontroller_MSPM0G3507,
   Microcontroller_MSPM33C3x,
   LEDDriver_TLC59116,
@@ -182,6 +185,7 @@ export const TiChipComponents = {
   MSP430G2230ID,
   MSP430G2332IPW20,
   MSP430F5229,
+  MSPM0L1306,
   MSPM33C3x,
   MSPM0G3507,
   SN65HVD1473,
@@ -233,6 +237,7 @@ export const TiSubcircuitComponents = {
   AudioAmplifier_TAS2505,
   TargetSocket_MSPTS430D8,
   BluetoothAudioHost_MSP430F5229,
+  Microcontroller_MSPM0L1306,
   Microcontroller_MSPM0G3507,
   Microcontroller_MSPM33C3x,
   LEDDriver_TLC59116,
